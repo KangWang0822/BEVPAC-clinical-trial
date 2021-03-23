142bd97a5cd3329eb0a654e7545e91ccf479131f
KangWang0822 <49832670+KangWang0822@users.noreply.github.com>
2021-03-23 20:47:15 +0100

    Update RNA-seq.sh

--- RNA-seq.sh
+++ RNA-seq.sh
@@ -3,11 +3,17 @@
 #Data deliver for NGI
 #Data location
 cd /proj/sens2019581/delivery04273/INBOX/P18362/
-#copy the results from NGI
-rsync -ah kangwang-sens2019581@bianca.uppmax.uu.se:/preoj/sens2019581/delivery04273/INBOX/P18362/00-Reports ~/
 
+#传输：https://www.uppmax.uu.se/support/user-guides/basic-sftp-commands/
+#Uploaded 
+sftp -q kangwang-sens2019581@bianca-sftp.uppmax.uu.se
+cd kangwang-sens2019581
+lls
+put hg38_chr.38  #上传
 
-
+#Download
+cd /proj/sens2019581/nobackup/wharf/kangwang/kangwang-sens2019581 #All files must be transferred through the wharf area of Bianca (download) 复制文件到该目录下
+ls
 
 
 #SBATCH
@@ -20,3 +26,33 @@ content.....
 Ctrl+D  #ending edition
 Vim name.sh # editing modify
  
+################################
+################################
+################################
+#!/bin/bash -l
+
+#SBATCH -A sens2019581
+#SBATCH -p core
+#SBATCH -n 16
+#SBATCH -t 01:00:00
+#SBATCH -J CrosscheckFingerprints 
+
+biopsy1="133"
+biopsy2="165"
+
+IN1=$(ls /proj/sens2019581/delivery04273/INBOX/P18362/01-RNA-Results/markDuplicates/P18362_${biopsy1}Aligned.sortedByCoord.out.markDups.bam)
+IN2=$(ls /proj/sens2019581/delivery04273/INBOX/P18362/01-RNA-Results/markDuplicates/P18362_${biopsy2}Aligned.sortedByCoord.out.markDups.bam)
+
+echo "Loading necessary modules..."
+module load bioinfo-tools picard/2.23.4
+
+echo "Running CrosscheckFingerprints for the paired samples ${biopsy1} and ${biopsy2}..."
+java -jar $PICARD_ROOT/picard.jar CrosscheckFingerprints \
+           INPUT=${IN1} \
+           INPUT=${IN2} \
+           HAPLOTYPE_MAP=/home/emmasi/tools/CrosscheckFingerprints/haplotype_map/hg38_chr.map \
+           LOD_THRESHOLD=-5 \
+	   NUM_THREADS=16 \
+           EXPECT_ALL_GROUPS_TO_MATCH=true \
+           OUTPUT=/home/kangwang/tools/CrosscheckFingerprints/sample.crosscheck_metrics_${biopsy1}_vs${biopsy2}
+echo "Done..."
