0598fa0360c2e0e7fbb218f899fde570f5296de1
KangWang0822 <49832670+KangWang0822@users.noreply.github.com>
2021-03-23 19:17:06 +0100

    Create RNA-seq.sh

---
+++ RNA-seq.sh
@@ -0,0 +1,22 @@
+#BEVPAC clinical trial
+
+#Data deliver for NGI
+#Data location
+cd /proj/sens2019581/delivery04273/INBOX/P18362/
+#copy the results from NGI
+rsync -ah kangwang-sens2019581@bianca.uppmax.uu.se:/preoj/sens2019581/delivery04273/INBOX/P18362/00-Reports ~/
+
+
+
+
+
+#SBATCH
+squeue -u username
+sbatch name.slurm
+
+#creat a file and write bash script
+cat >name.sh
+content.....
+Ctrl+D  #ending edition
+Vim name.sh # editing modify
+ 
