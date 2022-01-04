9ab465dbc3ee558d46d1b9c9eb28fc4b8490452e
KangWang0822 <49832670+KangWang0822@users.noreply.github.com>
2022-01-04 14:30:09 +0100

    Update WES.sh

--- WES.sh
+++ WES.sh
@@ -109,11 +109,7 @@ nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow
 
 cd /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-results/Sarek-BEVPAC_WES
 
-#!/bin/bash -l
-#SBATCH -A sens2019581
-#SBATCH -p node -n 1 -C mem128GB
-#SBATCH -t 240:00:00
-#SBATCH -J sarak_test
+# Prepare recalibration
 module load bioinfo-tools Nextflow/21.04.1 nf-core/1.14 iGenomes/latest
 export NXF_OFFLINE='TRUE'
 export NXF_HOME="/castor/project/proj/nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/"
@@ -121,7 +117,6 @@ export PATH=${NXF_HOME}:${PATH}
 export NXF_TEMP=$SNIC_TMP
 export NXF_LAUNCHER=$SNIC_TMP
 export NXF_SINGULARITY_CACHEDIR="/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/"
-# Prepare recalibration
 nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/main.nf \
 --outdir "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-results/Sarek-BEVPAC_WES/results" \
 -profile uppmax \
@@ -136,19 +131,14 @@ nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow
 --save_bam_mapped \
 -resume jovial_sanger
 
-#!/bin/bash -l
-#SBATCH -A sens2019581
-#SBATCH -p node -n 1 -C mem128GB
-#SBATCH -t 240:00:00
-#SBATCH -J sarak_test
-module load bioinfo-tools Nextflow/21.04.1 nf-core/1.14
+# Recalibrate
+module load bioinfo-tools Nextflow/21.04.1 nf-core/1.14 iGenomes/latest
 export NXF_OFFLINE='TRUE'
 export NXF_HOME="/castor/project/proj/nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/"
 export PATH=${NXF_HOME}:${PATH}
 export NXF_TEMP=$SNIC_TMP
 export NXF_LAUNCHER=$SNIC_TMP
 export NXF_SINGULARITY_CACHEDIR="/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/"
-# Recalibrate
 nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/main.nf \
 -profile uppmax \
 -with-singularity "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/nf-core-sarek-2.7.1.simg" \
@@ -162,19 +152,14 @@ nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow
 --save_bam_mapped \
 -resume
 
-#!/bin/bash -l
-#SBATCH -A sens2019581
-#SBATCH -p node -n 1 -C mem128GB
-#SBATCH -t 240:00:00
-#SBATCH -J sarak_test
-module load bioinfo-tools Nextflow/21.04.1 nf-core/1.14
+# Variant calling: MuTect2
+module load bioinfo-tools Nextflow/21.04.1 nf-core/1.14 iGenomes/latest
 export NXF_OFFLINE='TRUE'
 export NXF_HOME="/castor/project/proj/nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/"
 export PATH=${NXF_HOME}:${PATH}
 export NXF_TEMP=$SNIC_TMP
 export NXF_LAUNCHER=$SNIC_TMP
 export NXF_SINGULARITY_CACHEDIR="/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/"
-# Variant calling: MuTect2
 nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/main.nf \
 -profile uppmax \
 -with-singularity "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/nf-core-sarek-2.7.1.simg" \
@@ -183,8 +168,8 @@ nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow
 --project sens2019581 \
 --input "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-results/Sarek-BEVPAC_WES/results/Preprocessing/TSV/recalibrated.tsv" \
 --genome GRCh38 \
---germline_resource "/castor/project/proj_nobackup/references/Homo_sapiens/GATK/GRCh38/Annotation/GermlineResource/gnomAD.r2.1.1.GRCh38.PASS.AC.AF.only.vcf.gz" \
---germline_resource_index "/castor/project/proj_nobackup/references/Homo_sapiens/GATK/GRCh38/Annotation/GermlineResource/gnomAD.r2.1.1.GRCh38.PASS.AC.AF.only.vcf.gz.tbi" \
+--germline_resource "$IGENOMES_DATA/Homo_sapiens/GATK/GRCh38/Annotation/GermlineResource/gnomAD.r2.1.1.GRCh38.PASS.AC.AF.only.vcf.gz" \
+--germline_resource_index "$IGENOMES_DATA/Homo_sapiens/GATK/GRCh38/Annotation/GermlineResource/gnomAD.r2.1.1.GRCh38.PASS.AC.AF.only.vcf.gz.tbi" \
 --pon "/castor/project/proj_nobackup/references/Homo_sapiens/GATK/GRCh38/Annotation/GATKBundle/1000g_pon.hg38.vcf.gz" \
 --pon_index "/castor/project/proj_nobackup/references/Homo_sapiens/GATK/GRCh38/Annotation/GATKBundle/1000g_pon.hg38.vcf.gz.tbi" \
 --step 'variant_calling' \
@@ -193,7 +178,61 @@ nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow
 -resume
 
 
+# Variant calling: Manta (first run Manta, then Strelka ;according to Best Practices)
+module load bioinfo-tools Nextflow/21.04.1 nf-core/1.14 iGenomes/latest
+export NXF_OFFLINE='TRUE'
+export NXF_HOME="/castor/project/proj/nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/"
+export PATH=${NXF_HOME}:${PATH}
+export NXF_TEMP=$SNIC_TMP
+export NXF_LAUNCHER=$SNIC_TMP
+export NXF_SINGULARITY_CACHEDIR="/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/"
+nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/main.nf \
+-profile uppmax \
+-with-singularity "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/nf-core-sarek-2.7.1.simg" \
+--custom_config_base "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/configs" \
+-c "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-data/sarek.custom.config" \
+--project sens2019581 \
+--input "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-results/Sarek-BEVPAC_WES/results/Preprocessing/TSV" \
+--genome GRCh38 \
+--step 'variant_calling' \
+--tools 'manta' \
+--target_bed "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/BEVPAC-data/Twist_Exome_RefSeq_targets_hg38_100bp_padding.bed" \
+-resume
 
+# Variant calling: Strelka (first run Manta, then Strelka ;according to Best Practices)
+module load bioinfo-tools Nextflow/21.04.1 nf-core/1.14 iGenomes/latest
+export NXF_OFFLINE='TRUE'
+export NXF_HOME="/castor/project/proj/nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/"
+export PATH=${NXF_HOME}:${PATH}
+export NXF_TEMP=$SNIC_TMP
+export NXF_LAUNCHER=$SNIC_TMP
+export NXF_SINGULARITY_CACHEDIR="/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/"
+nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/main.nf \
+-profile uppmax \
+-with-singularity "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/nf-core-sarek-2.7.1.simg" \
+--custom_config_base "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/configs" \
+-c "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-data/sarek.custom.config" \
+--project sens2019581 \
+--input "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-results/Sarek-BEVPAC_WES/results/Preprocessing/TSV" \
+--genome GRCh38 \
+--step 'variant_calling' \
+--tools 'strelka' \
+--target_bed "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/BEVPAC-data/Twist_Exome_RefSeq_targets_hg38_100bp_padding.bed" \
+-resume
+
+# Variant calling: CNVkit
+nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/main.nf \
+-profile uppmax \
+-with-singularity "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/nf-core-sarek-2.7.1.simg" \
+--custom_config_base "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/configs" \
+-c "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-data/sarek.custom.config" \
+--project sens2019581 \
+--input "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-results/Sarek-BEVPAC_WES/results/Preprocessing/TSV" \
+--genome GRCh38 \
+--step 'variant_calling' \
+--tools 'cnvkit' \
+--target_bed "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/BEVPAC-data/Twist_Exome_RefSeq_targets_hg38_100bp_padding.bed" \
+-resume
 
 
 
