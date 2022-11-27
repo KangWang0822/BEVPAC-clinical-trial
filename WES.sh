5e2852890a4c596d2866ab8b2896b6d729545ebc
WangKang-Leo <49832670+WangKang-Leo@users.noreply.github.com>
2022-11-27 15:13:36 +0100

    Update WES.sh

--- WES.sh
+++ WES.sh
@@ -539,7 +539,29 @@ nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow
 --save_bam_mapped \
 -resume 
 
-
+# Variant calling: MuTect2
+module load bioinfo-tools Nextflow/21.04.1 nf-core/1.14 iGenomes/latest
+export NXF_OFFLINE='TRUE'
+export NXF_HOME="/castor/project/proj/nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/"
+export PATH=${NXF_HOME}:${PATH}
+export NXF_TEMP=$SNIC_TMP
+export NXF_LAUNCHER=$SNIC_TMP
+export NXF_SINGULARITY_CACHEDIR="/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/"
+nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow/main.nf \
+-profile uppmax \
+-with-singularity "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/singularity-images/nf-core-sarek-2.7.1.simg" \
+--custom_config_base "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/configs" \
+--project sens2019581 \
+--input "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-results/Sarek-BEVPAC_WES/recalibrated_add.tsv" \
+--genome GRCh38 \
+--germline_resource "$IGENOMES_DATA/Homo_sapiens/GATK/GRCh38/Annotation/GermlineResource/gnomAD.r2.1.1.GRCh38.PASS.AC.AF.only.vcf.gz" \
+--germline_resource_index "$IGENOMES_DATA/Homo_sapiens/GATK/GRCh38/Annotation/GermlineResource/gnomAD.r2.1.1.GRCh38.PASS.AC.AF.only.vcf.gz.tbi" \
+--pon "/castor/project/proj_nobackup/references/Homo_sapiens/GATK/GRCh38/Annotation/GATKBundle/1000g_pon.hg38.vcf.gz" \
+--pon_index "/castor/project/proj_nobackup/references/Homo_sapiens/GATK/GRCh38/Annotation/GATKBundle/1000g_pon.hg38.vcf.gz.tbi" \
+--step 'variant_calling' \
+--tools 'mutect2' \
+--target_bed "/proj/nobackup/sens2019581/wharf/kangwang/kangwang-sens2019581/Twist_Comprehensive_Exome_Covered_Targets_GRCh38.pad.sort.merge.bed" \
+-resume 
 
 
 
