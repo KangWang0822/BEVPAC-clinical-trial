6de7b7da945eed492494413a4a28ef560271d65d
WangKang-Leo <49832670+WangKang-Leo@users.noreply.github.com>
2023-08-27 14:52:57 +0800

    Update PureCN.sh

--- PureCN.sh
+++ PureCN.sh
@@ -26,3 +26,77 @@ nextflow run /castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/workflow
 --tools 'mutect2' \
 --target_bed "/castor/project/proj_nobackup/nf-core2/nf-core-sarek-2.7.1/BEVPAC-data/Twist_Exome_RefSeq_targets_hg38_100bp_padding.bed" \
 -resume
+
+ln -s /proj/sens2019581/nobackup/nf-core2/nf-core-sarek-2.7.1/Sarek-results/Sarek-BEVPAC_WES/Mutect2_germline_added/results/VariantCalling/*vs*/Mutect2/Mutect2_filtered*vcf.gz /proj/sens2019581/nobackup/PureCN/data
+
+cd /proj/sens2019581/nobackup/PureCN/data
+
+for file in *.vcf.gz;
+do 
+ID=${file#Mutect2_filtered_}
+ID=${ID%%vs*}
+ID=$(basename $ID _).vcf
+echo unzip $ID
+gunzip -c "$file">"$ID"
+done
+
+###########################################
+###############PureCN scratch##############
+###########################################
+# Step1 reference files
+
+#!/bin/bash -l
+#SBATCH -A sens2019581
+#SBATCH -p node
+#SBATCH -n 2
+#SBATCH -t 30:00:00
+#SBATCH -J PureCN
+module load R/4.2.1
+module load R_packages/4.2.1
+export PURECN="/sw/apps/R_packages/4.2.1/bianca/PureCN/extdata"
+export OUT_REF="/proj/sens2019581/nobackup/PureCN/reference_files"
+Rscript $PURECN/IntervalFile.R --in-file /proj/sens2019581/nobackup/PureCN/reference_files/Twist_Exome_RefSeq_targets_hg38_100bp_padding.bed \
+    --fasta /sw/data/uppnex/ToolBox/hg38bundle/Homo_sapiens_assembly38.fasta \
+    --genome hg38 \
+    --mappability /proj/sens2019581/nobackup/PureCN/reference_files/GCA_000001405.15_GRCh38_no_alt_analysis_set_100.bw \
+    --out-file $OUT_REF/Twist_Exome_RefSeq_targets_hg38_intervals.txt
+
+# Step2 Coverage
+export PURECN="/sw/apps/R_packages/4.1.1/bianca/PureCN/extdata"
+export OUT="/proj/sens2019581/nobackup/PureCN/data"
+export OUT_REF="/proj/sens2019581/nobackup/PureCN/reference_files"
+#!/bin/bash -l
+#SBATCH -A sens2022005
+#SBATCH -p node
+#SBATCH -n 4
+#SBATCH -t 12:00:00
+#SBATCH -J coverage_normal
+module load bioinfo-tools
+module load R/4.2.1
+module load R_packages/4.2.1
+cd /proj/nobackup/sens2022005/data/PREDIX_HER2_WES/PureCN/scratch/data/Normal
+export PURECN="/sw/apps/R_packages/4.2.1/bianca/PureCN/extdata"
+export OUT_REF="/proj/sens2022005/nobackup/data/PREDIX_HER2_WES/PureCN/scratch/reference_files"
+export OUT="/proj/nobackup/sens2022005/data/PREDIX_HER2_WES/PureCN/scratch/data/Normal"
+Rscript $PURECN/Coverage.R --out-dir $OUT \
+    --bam /proj/nobackup/sens2022005/data/PREDIX_HER2_WES/PureCN/scratch/data/normal_bam.list \
+    --intervals $OUT_REF/Twist_Comprehensive_Exome_baits_hg38_intervals.txt \
+    --cores 64 --parallel
+
+#!/bin/bash -l
+#SBATCH -A sens2022005
+#SBATCH -p node
+#SBATCH -n 6
+#SBATCH -t 12:00:00
+#SBATCH -J coverage_baseline
+module load bioinfo-tools
+module load R/4.2.1
+module load R_packages/4.2.1
+cd /proj/nobackup/sens2022005/data/PREDIX_HER2_WES/PureCN/scratch/data/Baseline
+export PURECN="/sw/apps/R_packages/4.2.1/bianca/PureCN/extdata"
+export OUT_REF="/proj/sens2022005/nobackup/data/PREDIX_HER2_WES/PureCN/scratch/reference_files"
+export OUT="/proj/nobackup/sens2022005/data/PREDIX_HER2_WES/PureCN/scratch/data/Baseline"
+Rscript $PURECN/Coverage.R --out-dir $OUT \
+    --bam /proj/nobackup/sens2022005/data/PREDIX_HER2_WES/PureCN/scratch/data/baseline_bam.list \
+    --intervals $OUT_REF/Twist_Comprehensive_Exome_baits_hg38_intervals.txt \
+    --cores 96 --parallel --force
